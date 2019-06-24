578278114980ccbedbb9f812b2c1561710420bf4
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-06-24 18:26:26 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,7 +1,7 @@
-
 var first_number = "1"# Add your variables here
 var second_number = "2"
 sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
 quotient = first_number / second_number
+
